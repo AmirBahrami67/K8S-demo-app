be9d1bdc3274b27b350755a90c104f700b7682cb
amir <8112524+AmirBahrami67@users.noreply.github.com>
2021-04-21 21:10:50 -0400

    initial

--- src/main/java/com/abahrami/template/TemplateApplication.java
+++ src/main/java/com/abahrami/template/TemplateApplication.java
@@ -2,11 +2,48 @@ package com.abahrami.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class TemplateApplication { // TODO rename this class
+@RestController
+@RequestMapping("/demo")
+public class TemplateApplication {
 
-  public static void main(final String[] args) {
-    SpringApplication.run(TemplateApplication.class, args);
-  }
+    @GetMapping("/test")
+    public Map<String, String> test() throws Exception {
+        return Map.of(
+                "Host Address", InetAddress.getLocalHost().getHostAddress(),
+                "Host Name", InetAddress.getLocalHost().getHostName(),
+                "time", Calendar.getInstance().toInstant().toString()
+        );
+    }
+
+    @GetMapping("/addLoad/{min}")
+    public String addLoad(@PathVariable("min") int min) {
+        long start = System.currentTimeMillis();
+        ExecutorService es = Executors.newFixedThreadPool(100);
+        IntStream.rangeClosed(0, 100)
+                .forEach(x -> es.submit(() -> {
+                    for (; ; ) {
+                        if (System.currentTimeMillis() - start > min * 60 * 1000) break;
+                    }
+                    return 0;
+                }));
+
+        return "Load started " + Calendar.getInstance().toInstant().toString();
+    }
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TemplateApplication.class, args);
+    }
 }
